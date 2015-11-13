6b4e155f94f155f24d4c53515b37b05e6bd8db44
jpfeifer22 <jpfeifer22>
2015-11-13 17:49:49 +0000

    Add slackcurl.py

---
+++ Nexus9000/NXOS/guestshell/slackcurl.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import os
+import subprocess
+from nxapi_module import *
+
+response = donxapi('cli_show', 'show version')
+print response['body']['kickstart_ver_str']
+version = response['body']['kickstart_ver_str']
+chassis = response['body']['chassis_id']
+
+response = donxapi('cli_show', 'show hostname')
+hostname = response['body']['hostname']
+
+outj = "\"Hi, I am " + hostname + ", and I am a " + chassis
+
+cmd = "curl --proxy http://proxy.esl.cisco.com:8080 -X POST --data-urlencode 'payload={ \"channel\": \"#chat9k\", \"username\": \"" + hostname + "\", \"text\": " + outj + "\"}' https://hooks.slack.com/services/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"
+print "\n\n\n"
+print cmd
+print "\n\n\n"
+os.system(cmd)

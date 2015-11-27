2d327e57d2c417d15f7f2a07f35481ec82cdae9f
jpfeifer22 <jpfeifer@cisco.com>
2015-11-27 15:43:45 +0000

    Adjust payload.

--- Nexus9000/NXOS/guestshell/slackcurl.py
+++ Nexus9000/NXOS/guestshell/slackcurl.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-#This script will run on a 9k and push data into a slack webhook
-
 import os
 import subprocess
 from nxapi_module import *
@@ -10,11 +8,15 @@ response = donxapi('cli_show', 'show version')
 print response['body']['kickstart_ver_str']
 version = response['body']['kickstart_ver_str']
 chassis = response['body']['chassis_id']
+updays = response['body']['kern_uptm_days']
+uphrs = response['body']['kern_uptm_hrs']
+upmins = response['body']['kern_uptm_mins']
 
 response = donxapi('cli_show', 'show hostname')
 hostname = response['body']['hostname']
 
-outj = "\"Hi, I am " + hostname + ", and I am a " + chassis
+#outj = "\"Hi, I am " + hostname + ", and I am a " + chassis
+outj = "\"Hi, I am " + hostname + ", a " + chassis + ". I have been up for " + str(updays) + " days, " + str(uphrs) + " hours, and " + str(upmins) + " minutes."
 
 cmd = "curl --proxy http://proxy.esl.cisco.com:8080 -X POST --data-urlencode 'payload={ \"channel\": \"#chat9k\", \"username\": \"" + hostname + "\", \"text\": " + outj + "\"}' https://hooks.slack.com/services/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"
 print "\n\n\n"

199a6e76b453b28d699b35fb797fbe2333fbb0a1
cisco <cisco@acidemos.cisco.com>
2016-05-24 17:49:49 -0700

    Add listtenants_with_anps.py.

---
+++ Nexus9000/ACI/rest/listtenants_with_anps.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2015 Cisco Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script dumps out top level tenant information from ACI
+
+import requests
+import sys
+import json
+import logging
+import getpass
+from optparse import OptionParser
+
+requests.packages.urllib3.disable_warnings()
+
+# Gather CLI and interactive input options
+optp = OptionParser()
+optp.add_option("-i", "--IP", dest="IP",
+             help="IP address of APIC to connect to")
+optp.add_option("-u", "--USER", dest="USER",
+              help="Username")
+optp.add_option("-p", "--PASS", dest="PASS",
+              help="Password")
+opts, args = optp.parse_args()
+
+if opts.IP is None:
+     apic = raw_input("APIC IP Address: ")
+else:
+     apic = opts.IP
+
+if opts.USER is None:
+     user = raw_input("Username: ")
+else:
+     user = opts.USER
+
+if opts.PASS is None:
+     password = getpass.getpass("Password: ")
+else:
+     password = opts.PASS
+
+#import requests.packages.urllib3
+#requests.packages.urllib3.disable_warnings()
+
+tofile = ""
+#if len(sys.argv) < 2:
+#  tofile = ""
+#else:
+#  tofile = sys.argv[1]
+
+userJson = {'aaaUser': {'attributes': {'name': user, 'pwd': password } } }
+
+sess = requests.Session()
+
+rsp = sess.post('https://{0}/api/mo/aaaLogin.json'.format(apic), data=json.dumps(userJson, sort_keys=True, indent=4, separators=(',', ': ')), verify=False)
+
+rsp = sess.get('https://{0}/api/class/fvTenant.json'.format(apic), data=json.dumps(userJson, sort_keys=True, indent=4, separators=(',', ':')), verify=False)
+#formed = json.dumps(json.loads(rsp.content), indent=4, sort_keys=True) 
+
+formed = json.loads(rsp.content)
+#print formed['imdata'][0]['fvTenant']['attributes']['dn']
+for i in formed['imdata']:
+  tenant = i['fvTenant']['attributes']['dn']
+  print tenant
+
+  rsp = sess.get('https://{0}/api/node/mo/{1}.json?query-target=subtree&target-subtree-class=fvAp'.format(apic, tenant), data=json.dumps(userJson, sort_keys=True, indent=4, separators=(',', ':')), verify=False)
+  formed = json.loads(rsp.content)
+  for j in formed['imdata']:
+      anp = j['fvAp']['attributes']['dn'] 
+      print "    " + anp
+      rsp = sess.get('https://{0}/api/node/mo/{1}.json?query-target=subtree&target-subtree-class=fvAEPg'.format(apic, anp), data=json.dumps(userJson, sort_keys=True, indent=4, separators=(',', ':')), verify=False)
+      formed = json.loads(rsp.content)
+      for k in formed['imdata']:
+         epg = k['fvAEPg']['attributes']['dn'] 
+         print "        " + epg 
+
+
+
+
+  
+
+#for i in formed{imdata}: 
+#   print i
+#
+
+#if tofile == "":
+#  #print formed
+#else:
+#  with open(tofile, 'w') as f:
+#    f.write(formed)
+#    f.close()
+
+#print 'Please send ' + tofile + ' back to Cisco'

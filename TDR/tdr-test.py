3e2dccd11d692ab8899d4952b24ca58b441638ea
jpfeifer <jpfeifer@cisco.com>
2017-02-09 14:59:36 -0800

    Add first command.

--- TDR/tdr-test.py
+++ TDR/tdr-test.py
@@ -2,14 +2,6 @@
 
 import cli
 
-# o22-3850-10#sho  ip int brief | include Te.*up
-# Te1/1/2                unassigned      YES unset  up                    up    
-# o22-3850-10#sho  ip int brief | include Ethernet.*up     
-# GigabitEthernet0/0     172.26.170.243  YES manual up                    up    
-# GigabitEthernet1/0/1   unassigned      YES unset  up                    up    
-# o22-3850-10#
-
-
 o1 = cli.execute('show ip int brief | include Te.*up');
 o2 = cli.execute('show ip int brief | include Ethernet.*up');
 
@@ -35,4 +27,5 @@ for i in o2.split('\n'):
     grab_intf(i)
 
 for i in intfs:
-   print "i is ", i
+   cmd = "test cable-diagnostics tdr interface " + i
+   o3 = cli.execute(cmd);

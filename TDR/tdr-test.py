43ebd253d52b1b40865de71b16f6bde033ce319e
jpfeifer <jpfeifer@cisco.com>
2017-02-09 13:40:16 -0800

    more.

--- TDR/tdr-test.py
+++ TDR/tdr-test.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
 
-#import cli
-# print cli.execute('show version')
+import cli
 
 # o22-3850-10#sho  ip int brief | include Te.*up
 # Te1/1/2                unassigned      YES unset  up                    up    
-  
 # o22-3850-10#sho  ip int brief | include Ethernet.*up     
 # GigabitEthernet0/0     172.26.170.243  YES manual up                    up    
-  
 # GigabitEthernet1/0/1   unassigned      YES unset  up                    up    
-  
 # o22-3850-10#
 
-#o1 = cli.execute('show ip int brief | include Te.*up');
-#o2 = cli.execute('show ip int brief | include Ethernet.*up');
 
-o1 = ""
-o2 = "GigabitEthernet0/0/0   unassigned      YES NVRAM  up                    up\n" 
-o2+= "GigabitEthernet0/0/0   unassigned      YES NVRAM  up                    up\n"      
-o2+= "GigabitEthernet0       172.26.170.253  YES NVRAM  up                    up\n"      
-o2+= "GigabitEthernet0       172.26.170.253  YES NVRAM  up                    up\n"
+o1 = cli.execute('show ip int brief | include Te.*up');
+o2 = cli.execute('show ip int brief | include Ethernet.*up');
+
+#o1 = ""
+#o2 = "GigabitEthernet0/0/0   unassigned      YES NVRAM  up                    up\n" 
+#o2+= "GigabitEthernet0/0/0   unassigned      YES NVRAM  up                    up\n"      
+#o2+= "GigabitEthernet0       172.26.170.253  YES NVRAM  up                    up\n"      
+#o2+= "GigabitEthernet0       172.26.170.253  YES NVRAM  up                    up\n"
 
 intfs = set()
 
